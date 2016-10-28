82c7e1e2b2aaa1bc37ede84d06296503d3af99e1
astyer <88atti@gmail.com>
2016-10-27 21:55:29 -0400

    done challenge 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,37 +7,39 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        Scanner inputstring = new Scanner(System.in);
         System.out.println("Enter a grade");
         double grade = input.nextDouble();
+        System.out.println("Enter the student's name that got that grade.");
+        String name = inputstring.nextLine();
         double sum = 0;
         int x = 0;
         int num = 0;
         double grades [] = new double[100];
         String names [] = new String[100];
-        for(int i = 0; i < grades.length; i++)
-        {
-            grades [i] = -5;
-        }
 
         while(true)
         {
-            grades [x] = grade
+            grades [x] = grade;
+            names [x] = name;
             sum += grade;
             x++;
-            System.out.println("Enter a grade");
+            num++;
+            System.out.println("Enter a grade or -1 to stop.");
             grade = input.nextDouble();
             if(grade == -1.0)
             {
                 break;
             }
+            System.out.println("Enter the student's name that got that grade.");
+            name = inputstring.nextLine();
         }
-        for(int i = 0; i < grades.length; i++)
+        System.out.println("The list of student names is:");
+        for(int i = 0; i <= num; i++)
         {
-            if(grades [i] != -5)
-            {
-                num += 1;
-            }
+            System.out.println("Student " + (i+1) + ": " + names[i]);
         }
-        System.out.println(sum/num);
+        System.out.println("");
+        System.out.println("The average grade was: " + sum/num);
     }
 }
